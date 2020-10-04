e0bd14f5397d84fc707d8f58b8f9d8aa150858e9
steve <steve.ye.developer@gmail.com>
2020-10-04 17:17:50 +0900

    Add babel sample

---
+++ babel/jsx.js
@@ -0,0 +1,8 @@
+<div className="box">
+
+    <Title text="hello world" width={200}></Title>
+    <button onClick={() => {console.log("click")}}>좋아요</button>
+    <a href="/home" stype={{marginTop: '10px', color: 'red'}}>
+        홈으로 이동
+    </a>
+</div>
